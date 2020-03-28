bb083878c555b2d37f5b107b8b04fc11e3d28965
Mateusz <zsuetam.k@gmail.com>
2020-03-28 22:40:18 +0100

    zmieniona scena "Welcome": -Dodanie przycisku option Nieudolna próba effect volume Zepsucie music volume Dokonczenie pause menu, zmiany w option menu Zmiana muzyki w "Welcome"

--- Assets/scripts/quitGame.cs
+++ Assets/scripts/quitGame.cs
@@ -1,14 +1,12 @@
 ﻿
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class quitGame : MonoBehaviour
 {
     // Start is called before the first frame update
     public void QuitGame ()
     {
-        Debug.Log("quit");
-        Application.Quit();
-       
+        SceneManager.LoadScene("Welcome", LoadSceneMode.Single);
     }
 
     
